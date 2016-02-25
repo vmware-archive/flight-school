4458619c0f44e7c03e00e0808796b7a572c48df1
Chris Brown <xoebus@xoeb.us>
2016-02-25 10:09:03 -0800

    add views and data

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,19 @@
 require 'sinatra/base'
 
+require_relative "radar"
+
 class FlightSchool < Sinatra::Base
-  get '/' do
-    'Hello world!'
+  get "/" do
+    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "LAX"] }
+  end
+
+  get "/airports/:code" do
+    begin
+      airport_status = Radar.status_for(params[:code])
+      erb :airport, locals: { status: airport_status }
+    rescue Radar::NoSuchAirport
+      status 404
+      erb :no_airport
+    end
   end
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -11,9 +11,29 @@ RSpec.describe "The FlightSchool application" do
     FlightSchool
   end
 
-  it "says hello" do
+  before do
+    WebMock.allow_net_connect!
+  end
+
+  after do
+    WebMock.disable_net_connect!
+  end
+
+  it "shows a list of common airports" do
     get "/"
     expect(last_response).to be_ok
-    expect(last_response.body).to eq("Hello world!")
+    expect(last_response.body).to include("Current status of airports")
+  end
+
+  it "shows facts about a particular airport" do
+    get "/airports/SFO"
+    expect(last_response).to be_ok
+    expect(last_response.body).to include("San Francisco")
+  end
+
+  it "returns a 404 if the airport cannot be found" do
+    get "/airports/EDI"
+    expect(last_response).to be_not_found
+    expect(last_response.body).to include("Couldn't find that airport!")
   end
 end
