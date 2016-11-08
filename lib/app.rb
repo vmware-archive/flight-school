76ac4781da49f463f50472c9fb67888d432abe2e
Alvaro Guimaraes <alvaroguimaraes@gmail.com>
2016-11-08 17:24:51 +0100

    Break things.

--- lib/app.rb
+++ lib/app.rb
@@ -5,7 +5,7 @@ require_relative "radar"
 
 class FlightSchool < Sinatra::Base
   get "/" do
-    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "LAX"] }
+    erb :index, locals: { airports: ["SFO", "OHR", "FWA", "OAK", "SEA", "LAX"] }
   end
 
   get "/airports/:code" do
