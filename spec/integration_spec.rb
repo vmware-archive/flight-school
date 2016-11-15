cf2524787ee3cf01deb4a0f45c936adf8c67df46
Alexey Vakhov <vakhov@gmail.com>
2016-11-16 01:16:33 +0300

    saved_at_2016-11-16__01:16:33

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe "The FlightSchool application" do
 
   it "shows a list of common airports" do
     get "/"
-    expect(last_response).to be_ok
+    expect(last_response).to be_failed
     expect(last_response.body).to include("Current status of airports")
   end
 
