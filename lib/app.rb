0ea455f119e57d2498aa1feb098bda773e2b424c
Felix <felix@scl.co.uk>
2017-08-01 15:16:48 +0100

    wip7

--- lib/app.rb
+++ lib/app.rb
@@ -11,7 +11,7 @@ class FlightSchool < Sinatra::Base
   get "/airports/:code" do
     begin
       airport_status = Radar.status_for(params[:code])
-#      erb :airport, locals: { status: airport_status }
+      erb :airport, locals: { status: airport_status }
     rescue Radar::NoSuchAirport
       status 404
       erb :no_airport
@@ -20,12 +20,12 @@ class FlightSchool < Sinatra::Base
 
   get "/api/airports/:code" do
     begin
-#      airport_status = Radar.status_for(params[:code])
-#      json code: airport_status.code,
-#        name: airport_status.name,
-#        city: airport_status.city,
-#        weather: airport_status.weather
-#    rescue Radar::NoSuchAirport
+      airport_status = Radar.status_for(params[:code])
+      json code: airport_status.code,
+        name: airport_status.name,
+        city: airport_status.city,
+        weather: airport_status.weather
+    rescue Radar::NoSuchAirport
       status 404
     end
   end
